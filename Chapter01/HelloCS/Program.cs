01b26af5c5080f857f81d84e6d6828699cda55b3
Helena Sy <helsy@ukr.net>
2021-12-01 14:20:41 -0500

    examples from M.Price "C# 9 .NET 5 Core"

---
+++ Chapter01/HelloCS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace HelloCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int N = 777;
+            //int res = 0;
+            //string b = Convert.ToString(N, 2);
+            //int l = b.Length;
+            //for(int i=0; i<l; i++)
+            //{
+            //    if (b[i] == '0')
+            //    {
+            //        int j = i + 1;
+            //        while (j < l && b[j] == '0') j++;
+            //        if (j - i > res && j != l ) res = j - i;
+            //        i = j;
+            //    }
+            //}
+            //Console.WriteLine("Number = {0}, Binary = {1} Res = {2} ", N, b, res);
+            int[] A = new int[] { 3, 8, 9, 7, 6 };
+            int K = 3;
+            int l = A.Length;
+            if (K <= 0) return; // A;
+            for (int i = 0; i < K; i++)
+            {
+                int last = A[l - 1];
+                for (int j = l - 1; j > 0; j--)
+                    A[j] = A[j - 1];
+                A[0] = last;
+
+            }
+        }
+    }
+}
---
+++ Chapter01/TopLevelProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+Console.WriteLine("Hello World!");
+foreach(var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+{
+    var a = Assembly.Load(new AssemblyName(r.FullName));
+    int m = 0;
+    foreach(var t in a.DefinedTypes)
+        m+=t.GetMethods().Count();
+    Console.WriteLine("{0:N0} types with {1:N0} methods in {2} assembly.", a.DefinedTypes().Count(), m, r.Name);
+}
+
